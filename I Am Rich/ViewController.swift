79152c32a99b27c2bfc4a2cd6980f281325eb7a7
Anastasios <anastasios.rig@gmail.com>
2022-10-04 21:26:24 +1100

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Anastasios Rigatos on 4/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
